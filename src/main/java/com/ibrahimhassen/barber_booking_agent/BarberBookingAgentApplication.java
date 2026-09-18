f6ee7382e4092f74e90b8b69c2dfa786f3567c3a
Ibrahim Hassen <ibra.o.hassen@gmail.com>
2026-09-17 21:35:09 -0400

    Initialize Spring Boot backend

---
+++ src/main/java/com/ibrahimhassen/barber_booking_agent/BarberBookingAgentApplication.java
@@ -0,0 +1,13 @@
+package com.ibrahimhassen.barber_booking_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberBookingAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberBookingAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibrahimhassen/barber_booking_agent/BarberBookingAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibrahimhassen.barber_booking_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberBookingAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
